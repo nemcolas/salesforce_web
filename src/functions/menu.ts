ba7632dd663c5b7f65cc0a37dd6f1095589f7d3e
yagoluucas <yago543@gmail.com>
2024-03-25 17:20:19 -0300

    feito função que melhora o menu mobile e a navegação por tab

--- src/functions/menu.ts
+++ src/functions/menu.ts
@@ -1,7 +1,8 @@
 'use client'
 
 function criarMenu(linksMenu: Object, iconFecharMenu: HTMLImageElement) {
-    const menuMobile:HTMLDivElement = document.createElement('div');
+    const menuMobile: HTMLDivElement = document.createElement('div');
+    menuMobile.tabIndex = -1;
     const ulMenuMobile: HTMLUListElement = document.createElement('ul');
     for (const [texto, href] of Object.entries(linksMenu)) {
         const liMenuMobile: HTMLLIElement = document.createElement('li');
@@ -24,8 +25,9 @@ function mostrarMenu() {
             'Indústrias': 'https://www.salesforce.com/br/solutions/industries/',
             'Aprendizado': 'https://trailhead.salesforce.com/pt-BR',
             'Suporte': 'https://help.salesforce.com/s/',
-            'Empresa': 'https://www.salesforce.com/br/company/our-story/'
-        }  
+            'Empresa': 'https://www.salesforce.com/br/company/our-story/',
+            'Contato': 'https://www.salesforce.com/br/form/contact/contactme/?d=cta-header-9'
+        }
         const iconFecharMenu: HTMLImageElement = document.createElement('img');
         iconFecharMenu.classList.add('icon-fechar-menu');
         iconFecharMenu.setAttribute('tabIndex', '0');
@@ -36,22 +38,25 @@ function mostrarMenu() {
         menuMobile.classList.add('menu-mobile', 'adicionar-menu');
         headerPage.appendChild(menuMobile);
         iconFecharMenu.addEventListener('click', esconderMenu);
+        iconFecharMenu.addEventListener('keydown', esconderMenu);
+        menuMobile.focus();
     }
 }
 
-function esconderMenu() {
-    const iconAbrirMenu = document.querySelector('.icon-menu')
-    const iconFecharMenu = document.querySelector('.icon-fechar-menu');
-    const menuMobile = document.querySelector('.menu-mobile');
-    if (iconFecharMenu && iconAbrirMenu && menuMobile) {
-        iconAbrirMenu.classList.remove('hidden');
-        menuMobile.classList.remove('adicionar-menu');
-        menuMobile.classList.add('retirar-menu');
-        setTimeout(() => {
-            menuMobile.remove();
-        }, 700)        
+function esconderMenu(e:any) {
+    if (e.type == 'click' || e.key === 'Enter' || e.key === 'Escape' || e.key === 'Tab') {
+        const iconAbrirMenu = document.querySelector('.icon-menu')
+        const iconFecharMenu = document.querySelector('.icon-fechar-menu');
+        const menuMobile = document.querySelector('.menu-mobile');
+        if (iconFecharMenu && iconAbrirMenu && menuMobile) {
+            iconAbrirMenu.classList.remove('hidden');
+            menuMobile.classList.remove('adicionar-menu');
+            menuMobile.classList.add('retirar-menu');
+            setTimeout(() => {
+                menuMobile.remove();
+            }, 700)
+        }
     }
 }
-
 
 export { mostrarMenu };
