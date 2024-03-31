47ad3525f92cadc57b8c6513b2368754da81ec7c
yagoluucas <yago543@gmail.com>
2024-03-31 11:59:25 -0300

    excluindo arquivos que não estavam sendo utilizados

--- src/app/setTop.tsx
+++
@@ -1,8 +0,0 @@
-'use client' 
-import { usePathname } from 'next/navigation' 
-
-function setHeaderTop() {
-    return usePathname() === '/' || usePathname() === '/products' ? '' : 'hidden';
-}
-
-export { setHeaderTop }
