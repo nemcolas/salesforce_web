22353e45a7d8f6013638ba4b4f725d5430ea46dd
yagoluucas <yago543@gmail.com>
2024-05-10 23:20:37 -0300

    arrumado o footer da página

--- src/components/Footer/utils.ts
+++ src/components/Footer/utils.ts
@@ -4,7 +4,7 @@ export function getClassFooter(path: string){
     const mapClass: MapClass = {
         '/': 'footer-top flex',
         '/products': 'footer-top flex',
-        '/freetrial': 'footer-top flex',
+        '/freetrial': 'hidden',
     }
     return mapClass[path]
 }
