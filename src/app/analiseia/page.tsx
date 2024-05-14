4d529781c0cb88a36df38e93cb0b6f22487ae349
yagoluucas <yago543@gmail.com>
2024-05-14 08:28:14 -0300

    feito estrutura do formulário

---
+++ src/app/analiseia/page.tsx
@@ -0,0 +1,47 @@
+export default function AnaliseIa() {
+    return (
+        <main>
+            <h1>Análise de propensão</h1>
+            <form>
+                <h2>Preencha os dados abaixo para verificarmos se você é propenso a realizar nosso teste grátis</h2>
+
+                <section>
+                    <h3>De 0 a 5, o quanto voce sabe sobre o que e CRM?</h3>
+                    <input type="radio" name="sabe sobre crm" id="q1-1" value={1}/><label htmlFor="q1-1">1</label>
+                    <input type="radio" name="sabe sobre crm" id="q1-2" value={2}/><label htmlFor="q1-2">2</label>
+                    <input type="radio" name="sabe sobre crm" id="q1-3" value={3}/><label htmlFor="q1-3">3</label>
+                    <input type="radio" name="sabe sobre crm" id="q1-4" value={4}/><label htmlFor="q1-4">4</label>
+                    <input type="radio" name="sabe sobre crm" id="q1-5" value={5}/><label htmlFor="q1-5">5</label>
+                </section>
+
+                <section>
+                    <h3>De 0 a 5, o quanto voce conhece sobre Salesforce (Considere 0 como nao conheco e 5 como conheco muito)</h3>
+                    <input type="radio" name="sabe sobre salesforce" id="q2-1" value={1}/><label htmlFor="q2-1">1</label>
+                    <input type="radio" name="sabe sobre salesforce" id="q2-2" value={2}/><label htmlFor="q2-2">2</label>
+                    <input type="radio" name="sabe sobre salesforce" id="q2-3" value={3}/><label htmlFor="q2-3">3</label>
+                    <input type="radio" name="sabe sobre salesforce" id="q2-4" value={4}/><label htmlFor="q2-4">4</label>
+                    <input type="radio" name="sabe sobre salesforce" id="q2-5" value={5}/><label htmlFor="q2-5">5</label>
+                </section>
+
+
+                <section>
+                    <h3>Qual o tamanho da sua empresa? (Considere 1 como pequena empresa de 0 a 9 funcionarios, 2 como media empresa de 10 a 50 funcionarios e 3 como grande empresa, acima de 50 funcionarios)</h3>
+                    <input type="radio" name="tamanho empresa" id="q3-1" value={1}/><label htmlFor="q3-1">1</label>
+                    <input type="radio" name="tamanho empresa" id="q3-2" value={2}/><label htmlFor="q3-2">2</label>
+                    <input type="radio" name="tamanho empresa" id="q3-3" value={3}/><label htmlFor="q3-3">3</label>
+                </section>
+
+                <section>
+                    <h3>O Quão importante voce considera o uso de CRM para a sua empresa?</h3>
+                    <input type="radio" name="sabe sobre salesforce" id="q4-1" value={1}/><label htmlFor="q4-1">1</label>
+                    <input type="radio" name="sabe sobre salesforce" id="q4-2" value={2}/><label htmlFor="q4-2">2</label>
+                    <input type="radio" name="sabe sobre salesforce" id="q4-3" value={3}/><label htmlFor="q4-3">3</label>
+                    <input type="radio" name="sabe sobre salesforce" id="q4-4" value={4}/><label htmlFor="q4-4">4</label>
+                    <input type="radio" name="sabe sobre salesforce" id="q4-5" value={5}/><label htmlFor="q4-5">5</label>
+                </section>
+
+                <input type="button" value="Realizar análise" />
+            </form>
+        </main>
+    )
+}
